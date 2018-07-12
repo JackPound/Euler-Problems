9f984b7ea33f439cd9daae3e9752490380590aae
JackPound <jackpound@live.com>
2018-07-12 15:26:04 -0700

    added problem descriptions for 4-7

--- p007.py
+++ p007.py
@@ -1,3 +1,6 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+# What is the 10 001st prime number?
 def is_prime(number_to_check):
   prime = True
   for x in range (2, number_to_check):
@@ -17,4 +20,4 @@ def prime_position(at_position):
       count += 1
   print(prime_list[-1])
 
-prime_position(10001)
+prime_position(10001) 
